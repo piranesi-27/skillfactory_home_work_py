a8ca09ea459de21ffecd7aef9ee80c5e04c70e2c
piranesi-27 <somo1927@gmail.com>
2022-08-10 19:20:52 +0300

    четвертое обновление файла

--- home_work_12.7.3.py
+++ home_work_12.7.3.py
@@ -4,11 +4,11 @@ per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0} #сл
 
 values = list(per_cent.values()) #выделяем из словаря процентные ставки и заносим в список
 
-deposit = [int(money / 100 * values[0]), int(money / 100 * values[1]), int(money / 100 * values[2]), int(money / 100 * values[3])] #рассчитываем депозит- 
+deposit = [int(money / 100 * values[0]), int(money / 100 * values[1]), int(money / 100 * values[2]), int(money / 100 * values[3])] #рассчитываем депозит-
 #обращаясь к индексу ставки из списка, отбрасываем дробную часть и оборачиваем в список
 
-deposit_max = int(max(deposit)) #определяем максимальную сумму
+deposit_max = [int(max(deposit))] #определяем максимальную сумму
 
 print('deposit = ' + str(deposit)) #выводим все значения
 
-print('Максимальная сумма, которую вы можете заработать — ' + str(deposit_max)) #выводим максимальную сумму
+print('Максимальная сумма, которую вы можете заработать — ' + 'deposit' + str(deposit_max)) #выводим максимальную сумму
